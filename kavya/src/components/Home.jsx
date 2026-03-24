7c0e4f8d549e62e524a435650e220cdb317c43f2
arjun <arjunmvbabu@gmail.com>
2026-03-24 19:56:36 +0530

    5

--- kavya/src/components/Home.jsx
+++ kavya/src/components/Home.jsx
@@ -4226,6 +4226,22 @@ const Home = () => {
             <a href="/second">No</a>
             </div>
         </div> */}
+        {/* <div class="container">
+            <div class="tenor-gif-embed" data-postid="22885016" data-share-method="host" data-aspect-ratio="1.04918"
+            data-width="100%">
+            <a href="https://tenor.com/view/manja-gif-22885016">Ayyo stkr kanunille?</a>
+            <a href="https://tenor.com/search/manja-stickers">refresh adikk ponnu</a>
+            </div>
+            
+            <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+            <h1>പൊന്നൂ...Will you be my valentine? 😁</h1>
+            <p>എൻ്റെ പഞ്ചാരകട്ടി അല്ലെ..? 🥺</p>
+
+            <div class="btn">
+            <a href="/yes">Yes</a>
+            <a href="/second">No</a>
+            </div>
+        </div> */}
         <div class="container">
             <div class="tenor-gif-embed" data-postid="22885016" data-share-method="host" data-aspect-ratio="1.04918"
             data-width="100%">
